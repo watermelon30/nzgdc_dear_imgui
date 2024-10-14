769e7a8d2c5a9c6201e1c8ea25a769f2ad6a92b8
watermelon30 <jjack1226tw@gmail.com>
2024-10-14 21:27:33 +1300

    Push jsoncpp as normal folder

---
+++ third_party/jsoncpp/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/imgui_demo/nzgdc_dear_imgui/third_party/jsoncpp
+# Build directory: E:/imgui_demo/nzgdc_dear_imgui/third_party/jsoncpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("include")
---
+++ third_party/jsoncpp/build/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/imgui_demo/nzgdc_dear_imgui/third_party/jsoncpp/include
+# Build directory: E:/imgui_demo/nzgdc_dear_imgui/third_party/jsoncpp/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
